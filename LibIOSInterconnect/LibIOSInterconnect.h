5b63a2c704d3b5c7260cb4c5653c430dce15f6ab
Dallin Lauritzen <dallin.lauritzen@gmail.com>
2013-01-12 09:02:40 -0700

    Initial Commit

---
+++ LibIOSInterconnect/LibIOSInterconnect.h
@@ -0,0 +1,13 @@
+//
+//  LibIOSInterconnect.h
+//  LibIOSInterconnect
+//
+//  Created by Dallin Lauritzen on 1/12/13.
+//  Copyright (c) 2013 Dallin Lauritzen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LibIOSInterconnect : NSObject
+
+@end
---
+++ LibIOSInterconnect/LibIOSInterconnect.m
@@ -0,0 +1,13 @@
+//
+//  LibIOSInterconnect.m
+//  LibIOSInterconnect
+//
+//  Created by Dallin Lauritzen on 1/12/13.
+//  Copyright (c) 2013 Dallin Lauritzen. All rights reserved.
+//
+
+#import "LibIOSInterconnect.h"
+
+@implementation LibIOSInterconnect
+
+@end
